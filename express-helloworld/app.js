d22d045a5ed9b510dd0bf8673a0ef9276e47eb94
pndhkm <72283525+pndhkm@users.noreply.github.com>
2022-12-15 14:54:10 +0700

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
